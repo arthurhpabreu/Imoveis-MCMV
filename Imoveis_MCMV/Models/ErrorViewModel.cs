c9078bd51e782658346ce67aaea7b4357464f087
Arthur Henrique de Paula Abreu <arthurhenriquedepaula@gmail.com>
2024-04-29 19:44:13 -0300

    primeiro commit

---
+++ Imoveis_MCMV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Imoveis_MCMV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
